8914f0a52a664fea6538eeb1b6d5f3e64bc77a71
Emma Mascillaro <emascillaro@gmail.com>
2022-02-04 21:14:25 -0500

    finish image to text

--- image_to_text.py
+++ image_to_text.py
@@ -2,12 +2,15 @@ import cv2
 from image_capture import Capture_image
 import pytesseract
 
-# Takes image
-Capture_image()
+def image_to_text_string():
+    # Takes image
+    Capture_image()
 
-# Reads Image
-img = cv2.imread("Capture_Image.jpg")
+    # Reads Image
+    img = cv2.imread("Capture_Image.jpg")
 
-# Converts Image to String
-text = pytesseract.image_to_string(img)
-print(text)
+    # Converts Image to String
+    text = pytesseract.image_to_string(img)
+    #print(text)
+
+    return text
---
+++ runner.py
@@ -0,0 +1,15 @@
+import cv2
+import pytesseract
+import os
+
+from image_capture import Capture_image
+from image_to_text import image_to_text_string
+
+
+text = image_to_text_string().lower()
+print(text)
+
+
+for i in text:
+    print(f"The current character is {i}")
+    os.system(f"arduino --upload {i}.ino --port /dev/ttyUSB*")
